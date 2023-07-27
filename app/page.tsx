f10ce8e49a82e19fc88f1ee1e80359240e5509c1
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-26 22:12:46 -0700

    Add Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 import Navbar from "@/components/Navbar";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
 
 	return (
 		<main>
-			<h1 className="text-white text-4xl">Netflix Clone</h1>
-			<pre className="text-white">{JSON.stringify(session, null, 2)}</pre>
 			<Navbar />
 		</main>
 	);
---
+++ components/NavbarItem.tsx
@@ -0,0 +1,7 @@
+type Props = {
+	label: string;
+};
+
+export default function NavbarItem({ label }: Props) {
+	return <div className="text-white cursor-pointer hover:text-gray-300 transition">{label}</div>;
+}
