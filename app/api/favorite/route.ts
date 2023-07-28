4e1a2fb2834997df47edcc4903a3b579f610a662
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-27 20:53:33 -0700

    Add API for adding an movie to favorite list

--- app/api/favorite/route.ts
+++ app/api/favorite/route.ts
@@ -2,11 +2,61 @@ import { prisma } from "@/lib/db";
 import serverAuth from "@/lib/serverAuth";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST(request: Request) {
 	try {
 		const { currentUser } = await serverAuth();
+		const { movieId }: { movieId: string } = await request.json();
 
-		const favoriteMovies = await prisma.movie.findMany();
+		const existingMovie = await prisma.movie.findUnique({
+			where: {
+				id: movieId,
+			},
+		});
+
+		if (!existingMovie) {
+			throw new Error("Invalid ID");
+		}
+
+		const user = await prisma.user.update({
+			where: { email: currentUser.email || undefined },
+			data: {
+				favoriteIds: {
+					push: movieId,
+				},
+			},
+		});
+
+		return NextResponse.json(user, { status: 201 });
+	} catch (error) {
+		return NextResponse.json({ error }, { status: 400 });
+	}
+}
+
+export async function DELETE(request: Request) {
+	try {
+		const { currentUser } = await serverAuth();
+		const { movieId }: { movieId: string } = await request.json();
+
+		const existingMovie = await prisma.movie.findUnique({
+			where: {
+				id: movieId,
+			},
+		});
+
+		if (!existingMovie) {
+			throw new Error("Invalid ID");
+		}
+
+		const updateFavoriteIds = currentUser.favoriteIds.filter((id) => id !== existingMovie.id);
+
+		const updatedUser = await prisma.user.update({
+			where: { email: currentUser.email || undefined },
+			data: {
+				favoriteIds: updateFavoriteIds,
+			},
+		});
+
+		return NextResponse.json(updatedUser, { status: 200 });
 	} catch (error) {
 		return NextResponse.json({ error }, { status: 400 });
 	}
